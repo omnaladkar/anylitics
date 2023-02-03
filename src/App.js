7daf89264b204244c18b94ee7bdcaa196adfaa74
omnaladkar <om.naladkar@gmail.com>
2023-02-03 11:14:27 +0530

    sdfghj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState,useEffect } from 'react';
+import "./App.css";
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import axios from "axios";
+import UserList from './containers/User'
+import User from './containers/UserDetails'
 function App() {
+
+  const [users, setUsers] = useState(null);
+
+
+  const rootURL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const result = await axios(
+        rootURL,
+      );
+ 
+      setUsers(result.data);
+    };
+ 
+    fetchUsers();
+  }, []);
+  if (!users) return null
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+    <div className="main-body">
+      <Routes>
+      <Route exact path="/" element={<UserList users={users}/>} />
+     
+     
+      <Route path="/users/:id" element={<User users={users} />} />
+      </Routes>
+   
+      </div>
+      </div>
+      </BrowserRouter>
+
+  
   );
 }
 
